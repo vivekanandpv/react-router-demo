96bb53271b840f240e1307feddce5ba7777c1a60
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-28 15:04:25 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-import Home from './Home';
-import Calculator from './Calculator';
-import NotFound from './NotFound';
-import Travel from './Travel';
-import BreakingNews from './BreakingNews';
-import NationalNews from './NationalNews';
-import WorldNews from './WorldNews';
-import NewsNotFound from './NewsNotFound';
+import { BrowserRouter as Router } from 'react-router-dom';
 import PrimaryRoutes from './PrimaryRoutes';
 
-function App(props) {
+function App() {
   return (
     <React.Fragment>
       <Router>
