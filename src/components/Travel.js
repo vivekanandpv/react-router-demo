cc3ee81d14d7ee1021817c5694af9a1c277ff066
Vivekanand P V <vivekanandpv@outlook.com>
2021-11-17 10:19:18 +0530

    changed let to const

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const Travel = props => {
   console.log(props.match); //  check this as well
-  let { year, month, day } = useParams(); //  more convenient way
+  const { year, month, day } = useParams(); //  more convenient way
   return (
     <React.Fragment>
       <p>Year: {year}</p>
