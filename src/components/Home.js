43a4669f3fa98ec16f6a8f5c50b200736bc1af6c
Vivekanand P V <vivekanandpv@outlook.com>
2022-03-22 15:31:49 +0530

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Home = props => {
+const Home = (props) => {
+  const history = useHistory();
+
+  const navigationHandler = () => {
+    history.push('/travel/2021/04/28');
+  };
+
   return (
     <React.Fragment>
       <div className='jumbotron jumbotron-fluid'>
         <div className='container'>
           <h1 className='display-4'>Home</h1>
           <p className='lead'>This is home.</p>
+          <button className='btn btn-primary' onClick={navigationHandler}>
+            To Travel
+          </button>
         </div>
       </div>
     </React.Fragment>
